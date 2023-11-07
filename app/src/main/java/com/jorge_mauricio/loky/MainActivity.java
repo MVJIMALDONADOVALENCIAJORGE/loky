20cbd8f4d57751cd688fdc9680f26c3dc33f24c8
JorgeMaldonado <100388671+MVJIMALDONADOVALENCIAJORGE@users.noreply.github.com>
2023-11-06 21:45:14 -0500

    creandoLoginpta1

---
+++ app/src/main/java/com/jorge_mauricio/loky/MainActivity.java
@@ -0,0 +1,28 @@
+package com.jorge_mauricio.loky;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+     TextView regis;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        regis = findViewById(R.id.Registar);
+
+        regis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent irRegistro = new Intent(MainActivity.this, Registro.class);
+                MainActivity.this.startActivity(irRegistro);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/jorge_mauricio/loky/Registro.java
@@ -0,0 +1,14 @@
+package com.jorge_mauricio.loky;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Registro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+    }
+}
