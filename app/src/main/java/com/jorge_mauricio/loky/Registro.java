2408333a28388b5ed19790286c4bbed2734d9f42
JorgeMaldonado <100388671+MVJIMALDONADOVALENCIAJORGE@users.noreply.github.com>
2023-11-16 22:23:23 -0500

    detalles

--- app/src/main/java/com/jorge_mauricio/loky/Registro.java
+++ app/src/main/java/com/jorge_mauricio/loky/Registro.java
@@ -6,16 +6,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.Firebase;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -66,6 +71,8 @@ public class Registro extends AppCompatActivity {
 
                            }
                        });
+               //traer datos
+              
            }
        });
 
